c4da2c8cbacc2706f30097d567c6fbf5ea1e44ae
hao552370074 <552370074@qq.com>
2020-03-25 13:57:59 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  configureWebpack: {
+    devServer: {
+      port: 8080,
+      open: true
+    }
+  }
 };
